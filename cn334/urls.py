33199c2d40f961412957b7e1ac08f08006658f3d
cchompoottr <chompoo99075@gmail.com>
2024-01-25 17:18:47 +0700

    Create page views

--- cn334/urls.py
+++ cn334/urls.py
@@ -20,10 +20,16 @@ from django.urls import path, include
 from ecommerce import views as ecom_views 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('ecommerce/', ecom_views.ecommerce_index_view),
+    path('ecommerce/item/<item_id>', ecom_views.item_view),
     
-    path("admin/", admin.site.urls),
-    path("ecommerce/", ecom_views.ecommerce_index_view),
-    path("ecommerce/item/<item_id>", ecom_views.item_view)
+    
+    path('ecommerce/homepage', ecom_views.homepage_view),
+    path('ecommerce/contacts', ecom_views.contacts_view),
+    path('ecommerce/categories', ecom_views.categories_view),
+    path('ecommerce/products', ecom_views.products_view),
+    path('ecommerce/checkout', ecom_views.checkout_view)
 ]
 
 
